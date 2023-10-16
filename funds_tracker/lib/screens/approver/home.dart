42d3af626a99de38cd4a5cb3fdbdfa9a02d623a2
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-10-16 11:02:19 +0200

    Updated home.dart file

--- funds_tracker/lib/screens/approver/home.dart
+++ funds_tracker/lib/screens/approver/home.dart
@@ -5,39 +5,37 @@ class RequesterHome extends StatelessWidget {
   Widget build(BuildContext ctx) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(children: [
-          Text('Hello John'),
-          Icon(Icons.notifications)
-        ]),
-        floatingActionButton: FloatingActionButton(
-          child: IconButton(
-            icon: Icon(Icons.add),
+          body: Row(children: [Text('Hello John'), Icon(Icons.notifications)]),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {},
+            backgroundColor: Colors.blueAccent,
           ),
-          color: Colors.blueAccent,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(
-                icon: Icon(Icons.home_rounded),
-                label: ''),
-            NavigationDestination(
-                icon: Icon(Icons.history_rounded),
-                label: ''),
-            NavigationDestination(
-                icon: Icon(Icons.person_outline_rounded),
-                label: ''),
-          ],
-          selectedIndex: currentPageIndex,
-          onDestinationSelected: (int index){
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-          backgroundColor: Colors.white,
-          animationDuration: Duration(milliseconds: 1000),
-        )
-      ),
+          bottomNavigationBar: NavigationBar(
+            destinations: [
+              NavigationDestination(
+                  selectedIcon: Icon(Icons.home_rounded),
+                  icon: Icon(Icons.home_rounded),
+                  label: ''),
+              NavigationDestination(
+                  selectedIcon: Icon(Icons.history),
+                  icon: Icon(Icons.history_rounded),
+                  label: ''),
+              NavigationDestination(
+                  selectedIcon: Icon(Icons.person),
+                  icon: Icon(Icons.person_outline_rounded),
+                  label: ''),
+            ],
+            //selectedIndex: currentPageIndex,
+            //onDestinationSelected: (int index){
+            //  setState(() {
+            //    currentPageIndex = index;
+            //  });
+            //},
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            backgroundColor: Colors.white,
+            animationDuration: Duration(milliseconds: 1000),
+          )),
     );
   }
 }
@@ -50,10 +48,11 @@ class Request {
   String payer;
   String status;
 
-  Request({this.id = 0,
-    this.requesterName = 'Bruce',
-    this.description = 'Transport',
-    this.amount = 10000,
-    this.payer = 'ACLIS',
-    this.status = 'Pending'}) {}
+  Request(
+      {this.id = 0,
+      this.requesterName = 'Bruce',
+      this.description = 'Transport',
+      this.amount = 10000,
+      this.payer = 'ACLIS',
+      this.status = 'Pending'}) {}
 }
