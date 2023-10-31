6c9f78c9c508c82d649876e9aa13bee2d73b3c5b
Alain Bruce Nishimwe <nishalbruce@gmail.com>
2023-10-31 15:17:40 +0200

    Integrated fontify

---
+++ funds_tracker/lib/components/request_list_tile.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class RequestListTile extends StatefulWidget {
+  //final requestName;
+  //final date;
+  //final time;
+//
+  //const RequestListTile({
+  //  super.key,
+  //  required this.requestName,
+  //  required this.date,
+  //  required this.time
+  //});
+
+  @override
+  State<RequestListTile> createState() => _RequestListTileState();
+}
+
+class _RequestListTileState extends State<RequestListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 12.0),
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+                borderRadius: BorderRadius.circular(5)
+            ),
+            height: 57,
+            child: const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Request #1",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700
+                    ),
+                  ),
+
+                  //SizedBox(
+                  //  height: 5,
+                  //),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                          "12/10/2023",
+                        style: TextStyle(
+                          fontSize: 13.3
+                        ),
+                      ),
+                      Text(
+                        "02:30 PM",
+                        style: TextStyle(
+                            fontSize: 13.3
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- funds_tracker/lib/screens/requester/home.dart
+++ funds_tracker/lib/screens/requester/home.dart
@@ -1,14 +1,147 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import '../../components/request_list_tile.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
-      ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 10,
+          backgroundColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+              backgroundColor: Colors.white,
+                icon: Icon(
+                  Icons.home_rounded,
+                  color: Color(0xFF40A6DD),
+                  size: 24,
+                ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.history),
+                label: ""
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: ""
+            )
+          ]),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "👋🏻 Hello, John",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+
+                    Icon(
+                      Icons.notifications,
+                    ),
+
+                    //Container(
+                    //  decoration: BoxDecoration(
+                    //    color: Colors.grey[300],
+                    //    borderRadius: BorderRadius.circular(12),
+                    //  ),
+                    //  padding: EdgeInsets.all(12),
+                    //  child: Icon(
+                    //    Icons.notifications,
+                    //  ),
+                    //)
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Recent Requests",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700),
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                Column(
+                  children: [
+                    RequestListTile(),
+                    RequestListTile(),
+                    RequestListTile(),
+                  ],
+                ),
+
+                TextButton(
+                    onPressed: () {},
+                    child:
+                    const Row(
+                      children: [
+                        Text(
+                          "View All",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.black
+                          ),
+                        ),
+                        Icon(
+                            Icons.arrow_forward_ios_rounded,
+                          size: 12,
+                          color: Colors.black,
+                        )
+                      ],
+                    ),
+                ),
+
+                const Text(
+                  "Pending Requests",
+                  style: TextStyle(
+                      color: Color(0xFFF29339),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700),
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                Expanded(
+                  child: ListView(
+                    children: [
+                      RequestListTile(),
+                      RequestListTile(),
+                      RequestListTile(),
+                      RequestListTile(),
+                      RequestListTile(),
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      
     );
   }
 }
